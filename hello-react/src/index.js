59ac04b7ec9b50ed9e4afbb381223eb872b4d9af
Stephen Printup <corky@corkysdevshop.com>
2020-07-15 17:31:52 -0600

    useState as status to reflect state of application

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -1,43 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react'; // incorporating useState hook. A hook is a function that allows you to add functionality to a component
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Lake() {
-  return (
-    <h1>Lake!</h1>
-  );
-}
 
-function SkiResort() {
-  return (
-    <h1>SkiResort!</h1>
-  );
-}
+function App(){
+  const [status] = useState("Open");
 
-function App({name}){
+  //Status will be a state value that reflects the current status for this application
   return (
-    //React.Fragments are intended to be used to deal with the issue of adjacent components
-    <>
-      <h1>{name}</h1>
-      <Lake />
-      <SkiResort />
-    </>
+    <div>
+      <h1>Status: {status}</h1> 
+    </div>
   );
 }
 // ReactDOM.render takes to arguments, first the argument is the element we want to create, then the second argument is where we want to render that element
-//when the app is rendered, add the property 'season' for summer, or winter
 ReactDOM.render(
-  <App name ="GraphQL"/>,
+  <App />,
   document.getElementById('root')
 );
-
-// this is a way to assign name to values instead of accessing by index
-// using commas as placeholders
-const [, , third] = [
-  "popcorn",
-  "pretzels",
-  "pineapple"
-]
-
-
-console.log(third); // pineapple
