d6b12d4f28f4bd32c366d7b514c6cecf4247ca11
Stephen Printup <corky@corkysdevshop.com>
2020-07-15 13:33:34 -0600

    rendering multiple elements

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -6,7 +6,8 @@ import './index.css';
 // ReactDOM.render takes to arguments, first the argument is the element we want to create, then the second argument is where we want to render that element
 ReactDOM.render(
   //React.createElement(tag that we want to create, properties we want this element to have, any children)
-  React.createElement("h1", {style:{color:"red"}}, "Hello!"),
+  // Possible to render multiple elements using createElement calls by making adjustments to the third argument (children)
+  React.createElement("div", {style:{color:"red"}}, React.createElement("h1",null,"hi!")),
   document.getElementById('root')
 );
 
