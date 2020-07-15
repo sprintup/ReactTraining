fad104904ba92f81337c7bfc8375a2184bf1cf23
Stephen Printup <corky@corkysdevshop.com>
2020-07-15 16:56:25 -0600

    passing in data to the child components

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -2,27 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Lake() {
+function Lake({name}) {
   return (
     <div>
-      <h1>Visit Jenny Lake!</h1>
+      <h1>Visit {name}!</h1>
     </div>
   );
 }
 
-function SkiResort() {
+function SkiResort({name}) {
   return (
     <div>
-      <h1>Visit Jackson Hole Mountain Resort!</h1>
+      <h1>Visit {name}!</h1>
     </div>
   );
 }
 
 function App(props){
   if(props.season === "summer"){
-    return <Lake />;
+    return <Lake name="Jenny Lake" />;
   } else if (props.season === "winter"){
-    return <SkiResort />;
+    return <SkiResort name="JHMR" />;
   }
 }
 // ReactDOM.render takes to arguments, first the argument is the element we want to create, then the second argument is where we want to render that element
