463650d03f63870b1ee51ba853bfe993c05f769d
Stephen Printup <corky@corkysdevshop.com>
2020-07-15 14:51:59 -0600

    first props example

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Just a place to practice React
 ## Hello React
 Folder with a create react project
-- https://reactjs.org/docs/create-a-new-react-app.html
+- [React Docs](https://reactjs.org/docs/create-a-new-react-app.html)
     - `npx create-react-app hello-react --use-npm`
-
+- [LinkedIn: Learning React.js](linkedin.com/learning/learning-react-js-5)
 ### React Developer Tools
--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Hello(){
+function Hello(props){
+  console.log(props);
   return (
     // Can wrap more than one tag in a set of paranthesis
     <div>
-      <h1>Welcome to React!</h1>
+      <h1>Welcome to {props.library}!</h1>
       <p>Let's build something cool.</p>
     </div>
   )
@@ -14,7 +15,9 @@ function Hello(){
 // ReactDOM.render takes to arguments, first the argument is the element we want to create, then the second argument is where we want to render that element
 ReactDOM.render(
   // A component is a function that returns some UI (collection of React Elements). The self closing Hello JSX tags below call the fuction above. Also, components have to be capitalized
-  <Hello />,
+  // Props (properties) is an object in React that contain properties about the component. Can display dynamic data within a component.
+  // 'React' library value accepts dynamic values (Next.js, Vue), so it'll update in the H1
+  <Hello library="React" />,
   document.getElementById('root')
 );
 
