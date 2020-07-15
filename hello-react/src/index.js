611b7afe751058ac4e3a4ff091f657db8f3242d9
Stephen Printup <corky@corkysdevshop.com>
2020-07-15 15:45:29 -0600

    composing multiple components

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -2,30 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Hello({library,message,number}){
+function Lake(props) {
+  return <h1>{props.name}</h1>
+}
+
+function App(){
   return (
-    // Can wrap more than one tag in a set of paranthesis
     <div>
-      <h1>Welcome to {library}!</h1>
-      <p>{message}</p>
-      <p>{number} Props Total</p> 
+      <Lake name="Lake Tahoe" />
+      <Lake name="Angora Lake" />
+      <Lake name="Shirley Lake" />
     </div>
   )
 }
 // ReactDOM.render takes to arguments, first the argument is the element we want to create, then the second argument is where we want to render that element
 ReactDOM.render(
-  // A component is a function that returns some UI (collection of React Elements). The self closing Hello JSX tags below call the fuction above. Also, components have to be capitalized
-  // Props (properties) is an object in React that contain properties about the component. Can display dynamic data within a component.
-  // 'React' library value accepts dynamic values (Next.js, Vue), so it'll update in the H1 (in this case React)
-  // "Have Fun! is rendered in the paragraph tag"
-  // Can pass functions to JSX Templates
-  // Common to see values from props object destructured for brevity
-  // React component as a function that takes in data as an argument and then returns react elements to creat a user interface.
-  <Hello 
-    library="React" 
-    message="add dynamic data!" 
-    number={3} 
-  />,
+  <App />,
   document.getElementById('root')
 );
 
