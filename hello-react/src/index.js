077868e493444e98a12adf9c1fd0a31fbf9f0db4
Stephen Printup <corky@corkysdevshop.com>
2020-07-15 16:54:26 -0600

    conditional rendering

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -2,27 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const lakeList = [
-  { id: "1", name: "Echo", trailhead: "Echo" },
-  { id: "2", name: "Maud", trailhead: "Wrights" },
-  { id: "3", name: "Velma", trailhead: "Bayview" },
-];
-
-const list = [1, 2, 3, 4, 5]; // How to add a key for a list of numbers
-
-function App({items, i}){
-  // working on removing the error about each child needing a unique key property. Need to add a key. Keys help React keep track of which items have changed (added, removed). A key is an identifier for a dynamically generated element. 
+function Lake() {
   return (
-    <ul>
-      {items.map((item, i) =>(
-        <li key={i}>{item}</li>
-      ))}
-    </ul>
-  )
+    <div>
+      <h1>Visit Jenny Lake!</h1>
+    </div>
+  );
+}
+
+function SkiResort() {
+  return (
+    <div>
+      <h1>Visit Jackson Hole Mountain Resort!</h1>
+    </div>
+  );
+}
+
+function App(props){
+  if(props.season === "summer"){
+    return <Lake />;
+  } else if (props.season === "winter"){
+    return <SkiResort />;
+  }
 }
 // ReactDOM.render takes to arguments, first the argument is the element we want to create, then the second argument is where we want to render that element
+//when the app is rendered, add the property 'season' for summer, or winter
 ReactDOM.render(
-  <App items={list}/>,
+  <App season="summer"/>,
   document.getElementById('root')
 );
 
