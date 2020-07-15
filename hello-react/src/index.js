31dd846d8b732a4444d92714735c51085f2fb6bb
Stephen Printup <corky@corkysdevshop.com>
2020-07-15 17:07:45 -0600

    div clutter before react fragment

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -2,41 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Lake({name}) {
+function Lake() {
   return (
-    <div>
-      <h1>Visit {name}!</h1>
-    </div>
+    <h1>Lake!</h1>
   );
 }
 
-function SkiResort({name}) {
+function SkiResort() {
   return (
-    <div>
-      <h1>Visit {name}!</h1>
-    </div>
+    <h1>SkiResort!</h1>
   );
 }
 
-function App(props){
+function App(){
   return (
     <div>
-      {(props.season === "summer") ? (
-        <Lake name="Jenny Lake" />
-      ) : props.season ==="winter" ? (
-        <SkiResort name="JHMR" />
-      ) : (
-        <h1>
-          Come back in the winter or summer!
-        </h1>
-      )}
+      <Lake />
+      <SkiResort />
     </div>
-  )
+  );
 }
 // ReactDOM.render takes to arguments, first the argument is the element we want to create, then the second argument is where we want to render that element
 //when the app is rendered, add the property 'season' for summer, or winter
 ReactDOM.render(
-  <App season="summer"/>,
+  <App />,
   document.getElementById('root')
 );
 
