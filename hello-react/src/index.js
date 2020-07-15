54c1939317838370566084aa90fe13cdcccfa986
Stephen Printup <corky@corkysdevshop.com>
2020-07-15 17:04:12 -0600

    ternary refactor of conditional

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -19,11 +19,19 @@ function SkiResort({name}) {
 }
 
 function App(props){
-  if(props.season === "summer"){
-    return <Lake name="Jenny Lake" />;
-  } else if (props.season === "winter"){
-    return <SkiResort name="JHMR" />;
-  }
+  return (
+    <div>
+      {(props.season === "summer") ? (
+        <Lake name="Jenny Lake" />
+      ) : props.season ==="winter" ? (
+        <SkiResort name="JHMR" />
+      ) : (
+        <h1>
+          Come back in the winter or summer!
+        </h1>
+      )}
+    </div>
+  )
 }
 // ReactDOM.render takes to arguments, first the argument is the element we want to create, then the second argument is where we want to render that element
 //when the app is rendered, add the property 'season' for summer, or winter
