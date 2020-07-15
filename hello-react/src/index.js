550976405109563a56b0476b66cb554cdc9573d7
Stephen Printup <corky@corkysdevshop.com>
2020-07-15 15:16:29 -0600

    example of passing function to JSX templates

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 function Hello(props){
-  console.log(props);
+  console.log(Object.keys(props)); // Object.keys( returns keys of the object -> [library, message, number])
   return (
     // Can wrap more than one tag in a set of paranthesis
     <div>
       <h1>Welcome to {props.library}!</h1>
       <p>{props.message}</p>
-      <p>{props.number} Props Total</p>
+      <p>{Object.keys(props).length} Props Total</p> 
     </div>
   )
 }
@@ -19,7 +19,12 @@ ReactDOM.render(
   // Props (properties) is an object in React that contain properties about the component. Can display dynamic data within a component.
   // 'React' library value accepts dynamic values (Next.js, Vue), so it'll update in the H1 (in this case React)
   // "Have Fun! is rendered in the paragraph tag"
-  <Hello library="React" message="Have Fun!" number={3}/>,
+  // Can pass functions to JSX Templates
+  <Hello 
+    library="React" 
+    message="Have Fun!" 
+    number={3} // "3 Props Total", unless you delete this line, then it is "2 Props Total"
+  />,
   document.getElementById('root')
 );
 
