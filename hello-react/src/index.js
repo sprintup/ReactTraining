8a276aa1cb15c2647cd780974ffc4f55a1f0934e
Stephen Printup <corky@corkysdevshop.com>
2020-07-15 13:51:35 -0600

    refactored with JSX

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -5,16 +5,15 @@ import './index.css';
 
 // ReactDOM.render takes to arguments, first the argument is the element we want to create, then the second argument is where we want to render that element
 ReactDOM.render(
-  //React.createElement(tag that we want to create, properties we want this element to have, any children)
+  // React.createElement(tag that we want to create, properties we want this element to have, any children)
   // Possible to render multiple elements using createElement calls by making adjustments to the third argument (children)
-  React.createElement(
-    "ul", 
-  {style:{color:"red"}}, 
-  React.createElement("li",null,"Hot Dogs"),
-  React.createElement("li",null,"Hamburgers"),
-  React.createElement("li",null,"Pizza"),
-  React.createElement("li",null,"Sushi"),
-  ),
+  // JSX (Javascript as XML) is a language extension that allows you to write tags directly in the javascript
+  <ul>
+    <li>Hot Dogs</li>
+    <li>Hamburgers</li>
+    <li>Pizza</li>
+    <li>Sushi</li>
+  </ul>,
   document.getElementById('root')
 );
 
