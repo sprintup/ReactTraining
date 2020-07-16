929edc9fc2fb47cc843bef5c3dee6399f6f3ca0b
Stephen Printup <corky@corkysdevshop.com>
2020-07-16 11:23:07 -0600

    displays stringified json data in browser

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -2,33 +2,25 @@ import React, { useState, useEffect } from 'react'; // incorporating useState ho
 import ReactDOM from 'react-dom';
 import './index.css';
 
+// the purpose of the GitHubUser is to fetch some data from Github API
+function GitHubUser({login}) {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${login}`)
+    .then(res => res.json())
+    .then(setData)
+    .catch(console.error);
+  }, []);
+  if(data){
+    return (
+      <div>{JSON.stringify(data)}</div>
+    )
+  }
+  return null;
+}
+
 function App() {
-  const [val, setVal] = useState("");
-  const [val2, setVal2] = useState("");
-
-  //useEffect will console log the value of each one of these fields
-  // the second argument sent to useEffect is called the dependency array. In the array you use the state variable that we want to listen for changes in. In other words this useEffect is only going to fire when the changing first value
-  useEffect(() => {
-    console.log(`field 1: ${val}`)
-  },[val]);
-
-  useEffect(() => {
-    console.log(`field 2: ${val2}`)
-  },[val, val2]);
-
-  return (
-    <>
-      <label>
-        Favorite Phrase:
-        <input value={val} onChange={e => setVal(e.target.value)}/>
-      </label>
-      <br />
-      <label>
-        Second Favorite Phrase:
-        <input value={val2} onChange={e => setVal2(e.target.value)}/>
-      </label>
-    </>
-  )
+  return <GitHubUser login="moonhighway" />
 }
 // ReactDOM.render takes to arguments, first the argument is the element we want to create, then the second argument is where we want to render that element
 ReactDOM.render(
