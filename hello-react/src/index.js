c01106fad0c2bee19403a6111071f7ad11472588
Stephen Printup <corky@corkysdevshop.com>
2020-07-15 18:11:04 -0600

    new manager sets value to Rachel

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -8,20 +8,23 @@ function App(){
   // useState returns a pair: 
   //  status = state value (Open) -> remmeber array destructuring is allowing us give this value a name
   //  second value is a function to update the state, we can call it setStatus
+
+
   const [manager, setManager] = useState("Alex");
   const [status, setStatus] = useState("Open");
 
   //Status will be a state value that reflects the current status for this application
   return (
     <>
-    <div>
-      <h1>Manager on Duty: {manager}</h1>
-    </div>
-    <div>
-      <h1>Status: {status}</h1> 
-      <button onClick={() => setStatus("Open")}>Open</button>
-      <button onClick={() => setStatus("Closed")}>Closed</button>
-    </div>
+      <div>
+        <h1>Manager on Duty: {manager}</h1>
+        <button onClick={() => setManager("Rachel")}> New Manger </button>
+      </div>
+      <div>
+        <h1>Status: {status}</h1> 
+        <button onClick={() => setStatus("Open")}>Open</button>
+        <button onClick={() => setStatus("Closed")}>Closed</button>
+      </div>
     </>
   );
 }
